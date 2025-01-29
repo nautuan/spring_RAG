af88be468ee1846b0e14bf2e68552ac0381db5d7
nautuan <tuan-truong@live.fr>
2025-01-29 17:11:42 +0100

    add chat prompt in the project

---
+++ src/main/java/fr/efrei/rag/service/AssistantAIService.java
@@ -0,0 +1,10 @@
+package fr.efrei.rag.service;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface AssistantAiService {
+    @SystemMessage(fromResource = "/prompts/system.st")
+    String chat(String userMessage);
+}
